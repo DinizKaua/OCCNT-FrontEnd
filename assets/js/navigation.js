00ff0826d7c9d76ee4b9e591d33ea617098baf04
DinizKaua <willykauadiniz@gmail.com>
2025-08-12 19:14:03 -0300

    OCCNT
    adicionei respostas nos cards de doenças

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -5,23 +5,33 @@ document.addEventListener('DOMContentLoaded', function() {
             const section = this.getAttribute('data-section');
             switch(section){
                 case 'dados':
-                    window.location.href = 'index.html';
+                    window.location.href = '/index.html';
                     break;
                     
                 case 'sobre':
-                    window.location.href = 'sobre.html';
+                    window.location.href = '/sobre.html';
                     break;
                 
                 case 'repositorio':
-                    window.location.href = 'repositorio.html';
+                    window.location.href = '/repositorio.html';
             }
         })
     })
 
-    const ctaButton = document.querySelector('.about-button');
-    if(ctaButton){
-        ctaButton.addEventListener('click', function(){
+    const aboutButton = document.querySelector('.about-button');
+    if(aboutButton){
+        aboutButton.addEventListener('click', function(){
             window.location.href = 'sobre.html';
         })
     }
+
+    const diseaseCard = document.querySelectorAll('.disease-card');
+    diseaseCard.forEach(card => {
+        card.addEventListener('click', function(){
+            const category = this.getAttribute('data-category');
+            window.location.href = `pages/categoria/${category}.html`;
+
+        })
+    })
+        
 })
